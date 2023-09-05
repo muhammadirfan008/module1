3fc4a67567eae51527a44351ef205dc9706f0210
Azx2 <126233598+Aziip@users.noreply.github.com>
2023-09-05 11:37:36 +0800

    s

--- moduleIrfan.py
+++ moduleIrfan.py
@@ -4,3 +4,7 @@ myModule.greeting("irfan")
 
 a = myModule.person1["age"]
 print(a)
+
+myModule.greeting("Azizi")
+l=myModule.person3['age']
+print(l)
--- myModule.py
+++ myModule.py
@@ -1,13 +1,18 @@
 def greeting(name):
     print("Hello," + name)
+
+
 person1 = {
-        "name" : "irfan",
-        "age" : 20,
-        "country" : "Malaysia"
-    }
+    "name": "irfan",
+    "age": 20,
+    "country": "Malaysia"
+}
 person2 = {
 
 }
 person3 = {
-
+    "name": "Azizi",
+    "age": "20",
+    "country": 'Malaysia'
 }
+
